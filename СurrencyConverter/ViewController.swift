2319df6886b23b75e8a09d4e5c4e9280ebb5603c
FoxyBooks <viktorya.fedosova2016@yandex.ru>
2022-03-19 10:36:06 +0300

    #81362 Improved button

--- СurrencyConverter/ViewController.swift
+++ СurrencyConverter/ViewController.swift
@@ -1,15 +1,28 @@
-//
+
 //  ViewController.swift
 //  СurrencyConverter
 //
 //  Created by Виктория Федосова on 16.03.2022.
 //
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-
+    
+@IBOutlet weak var dateButton: UIButton!
+let today = Date()
+let formatter1 = DateFormatter()
+var date: String?
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setButtonTitle()
+  }
+    
+    func setButtonTitle() {
+        formatter1.dateStyle = .short
+        date = formatter1.string(from: today)
+        dateButton.setTitle(date, for: UIControl.State.normal)
+        dateButton.titleLabel?.font = UIFont(name: "American Typewriter", size: 24)
     }
 }
