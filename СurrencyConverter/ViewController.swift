46d2917be43f37314432555aede0312dfe40ae33
FoxyBooks <viktorya.fedosova2016@yandex.ru>
2022-03-19 13:30:03 +0300

    #81362 Code refactoring

--- СurrencyConverter/ViewController.swift
+++ СurrencyConverter/ViewController.swift
@@ -9,20 +9,28 @@ import Foundation
 
 class ViewController: UIViewController {
     
-@IBOutlet weak var dateButton: UIButton!
-let today = Date()
-let formatter1 = DateFormatter()
-var date: String?
-   
+@IBOutlet private weak var dateButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setButtonTitle()
+        setupInitialState()
   }
     
-    func setButtonTitle() {
-        formatter1.dateFormat = "dd/MM/yyyy"
-        date = formatter1.string(from: today)
-        dateButton.setTitle(date, for: UIControl.State.normal)
-        dateButton.titleLabel?.font = UIFont(name: "American Typewriter", size: 24)
+    private func setupInitialState() {
+        setupDateButton()
     }
+    
+    private func setupDateButton() {
+        dateButton.setTitle(getCurrentDate(), for: UIControl.State.normal )
+    }
+    
+}
+
+private func getCurrentDate() -> String {
+    var currentDate: String
+    let today = Date()
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "dd/MM/yyyy"
+    currentDate = dateFormatter.string(from: today)
+   return currentDate
 }
